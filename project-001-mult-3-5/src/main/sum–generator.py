d23308211c1825767b3f8c27a59a8ed86535b568
JacksonDagger <JacksonDDagger@gmail.com>
2018-12-24 21:13:10 -0500

    Completed the challenge

--- project-001-mult-3-5/src/main/sum–generator.py
+++ project-001-mult-3-5/src/main/sum–generator.py
@@ -1,1 +1,24 @@
+#
+# generates the sum of all integers that are a multiple of 3 or 5 between 0 and the inputted max
+# @max: is a positive integer greater th
 
+
+def euler1(maximum):
+    sum = 0
+    prod3 = 0
+    prod5 = 0
+    while prod3 < maximum or prod5 < maximum:
+        if prod5 > prod3:
+            prod3 += 3
+            if prod3 < maximum and not(prod5 == prod3):
+                sum += prod3
+
+        else:
+            prod5 += 5
+            if prod5 < 1000:
+                sum += prod5
+
+    return sum
+
+
+print (euler1(1000))
