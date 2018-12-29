c47fce78090731aaa2c0f43374a5220eb11b35ee
JacksonDagger <JacksonDDagger@gmail.com>
2018-12-29 17:42:07 -0500

    Finished initial work on proj 11. Debugging and tests still needed

---
+++ project-011-grid-product/src/NotAGridException.py
@@ -0,0 +1,2 @@
+class NotAGridException
+    pass
--- project-011-grid-product/src/grid.py
+++ project-011-grid-product/src/grid.py
@@ -1,4 +1,10 @@
+import NotAGridException
+
+
 class grid:
+    #
+    # Constructs a grid with a file. Throws NotAGridException if the file doesn't contain a grid (all lines contains the same number of ints)
+    #
     def __init__(self, fname):
         f = open(fname, "r")
         if f.mode == 'r':
@@ -12,4 +18,111 @@ class grid:
                     intlist.append(int(y))
                 self.gridlist.append(intlist)
 
-        
+            self.width = len(self.gridlist[0])
+            for x in self.gridlist:
+                if len(x) != width
+                    raise NotAGridException.NotAGridException
+            self.height = len(self.gridlist)
+        else:
+            raise NotAGridException.NotAGridException
+
+
+    #
+    # Returns the largest product of adjacent numbers in a straight line in the grid from size numbers
+    #
+    def get_product(self, size):
+        if size > self.width and size > self.height:
+            return 0
+        retval = 0
+        for x in range(self.width):
+            for y in range(self.height):
+                temp = self.get_local_product(x, y, size)
+                if temp > retval:
+                    retval = temp
+        return retval
+
+    #
+    # Returns the grid's width
+    #
+    def get_width(self):
+        return self.width
+
+
+    #
+    # Returns the grid's height
+    #
+    def get_height(self):
+        return self.height
+
+
+    #
+    # Returns the largest adjacent product from the given coordinate
+    #
+    def get_local_product(self, x, y, size):
+        retval = 0
+        roomup = y + 1 - size >= 0
+        roomdn = y + size <= height
+        roomleft = x + 1 - size >= 0
+        roomright = x + size <= width
+
+        #up
+        if roomup:
+            temp = 1
+            for inc in range(size):
+                temp *= self.gridlist[y - inc][x]
+            if temp > retval:
+                retval = temp
+        #down
+        if roomdn:
+            temp = 1
+            for inc in range(size):
+                temp *= self.gridlist[y + inc][x]
+            if temp > retval:
+                retval = temp
+        #left
+        if roomleft:
+            temp = 1
+            for inc in range(size):
+                temp *= self.gridlist[y][x - inc]
+            if temp > retval:
+                retval = temp
+        #right
+        if roomright:
+            temp = 1
+            for inc in range(size):
+                temp *= self.gridlist[y][x + inc]
+            if temp > retval:
+                retval = temp
+        #upleft
+        if roomleft and roomup:
+            temp = 1
+            for inc in range(size):
+                temp *= self.gridlist[y - inc][x - inc]
+            if temp > retval:
+                retval = temp
+        #upright
+        if roomright and roomup:
+            temp = 1
+            for inc in range(size):
+                temp *= self.gridlist[y - inc][x + inc]
+            if temp > retval:
+                retval = temp
+        #downleft
+        if roomleft and roomdn:
+            temp = 1
+            for inc in range(size):
+                temp *= self.gridlist[y + inc][x - inc]
+            if temp > retval:
+                retval = temp
+        #downright
+        if roomright and roomdn:
+            temp = 1
+            for inc in range(size):
+                temp *= self.gridlist[y + inc][x + inc]
+            if temp > retval:
+                retval = temp
+
+        return retval
+
+
+
